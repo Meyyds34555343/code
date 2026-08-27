b8067b6b05b9039a3bddd78c397a820e978fc1a4
Meyyds34555343 <116258181+Meyyds34555343@users.noreply.github.com>
2026-08-27 15:07:14 +0800

    Time: 0 ms (100.00%) | Memory: 77.2 MB (12.82%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // Create a dummy head to simplify the logic
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        int carry = 0;
+        
+        // Traverse both linked lists
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            // Get values from current nodes (or 0 if null)
+            int val1 = (l1 != nullptr) ? l1->val : 0;
+            int val2 = (l2 != nullptr) ? l2->val : 0;
+            
+            // Calculate sum and new carry
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            int digit = sum % 10;
+            
+            // Create new node with the digit
+            current->next = new ListNode(digit);
+            current = current->next;
+            
+            // Move to next nodes
+            if (l1 != nullptr) l1 = l1->next;
+            if (l2 != nullptr) l2 = l2->next;
+        }
+        
+        // Return the head (skip the dummy node)
+        return dummy->next;
+    }
+};
