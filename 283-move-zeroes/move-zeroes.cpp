a032627abaf1b62dec7d773841f3b52dec77d779
Meyyds34555343 <116258181+Meyyds34555343@users.noreply.github.com>
2026-09-20 14:24:12 +0800

    Time: 0 ms (100.00%) | Memory: 23.8 MB (82.68%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int insertPos = 0;  
+        
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[insertPos++] = nums[i];
+            }
+        }
+        
+       
+        while (insertPos < nums.size()) {
+            nums[insertPos++] = 0;
+        }
+    }
+};
+
